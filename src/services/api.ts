b0f78f34f914b628cd7ad0b503b7b96331b99003
ibsiany <ibsiany.dias@seidor.com>
2024-08-27 08:41:24 -0300

    chore: remove env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// const url = process.env.API_URL_BACKEND
+const url = 'https://template-nodejs-nest-postgresql.onrender.com'
+
 export const api = axios.create({
-  baseURL: process.env.API_URL_BACKEND,
+  baseURL: url,
 });
